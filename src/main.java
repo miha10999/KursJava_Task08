4e3dbf44d56423ae150401a0546194a3e29014c2
mikhail <mihey10999@gmail.com>
2020-02-10 20:33:19 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+
+public class main  {
+
+    public static void main(String[] args) {
+        /* Задача №8: Цикличный вывод данных на экран.
+          Выполнил Хлюстов М.В.
+          Условие:Необходимо с помощью цикла вывести на экран числа от 1 до 50.
+        */
+    for (int i = 1; i<51;i++)
+        System.out.println("№" + i + " = " + i);
+        System.exit(0);// завершим выполнение программы
+    }
+    }
